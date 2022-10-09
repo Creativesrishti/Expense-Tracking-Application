04551be1e299056c8ab13eb463aafecfd161329b
Creativesrishti <creativesrishti@gmail.com>
2022-10-09 16:52:03 +0200

    added sort functionality and code to show expense/income/all depending on user input

--- Expense Tracking Application/Program.cs
+++ Expense Tracking Application/Program.cs
@@ -17,19 +17,57 @@ class Items   // base-parent-super class
     public double Amount { get; set; }
 
     static List<Items> expensesList = new List<Items>();
+    //location of file where data will be saved
     static string fileName = @"C:\Temp\Expenses.txt";
+
+    //method to list all the available items sorted by title, month and amount
     public static void showItems()
     {
-        Console.WriteLine("Id".PadRight(10) + "Title".PadRight(10) + "Month".PadRight(10)
-                + "Amount".PadRight(10));
-        foreach (Items items in expensesList)
+        Console.WriteLine("What do you want to see?(All/Expenses/Incomes)");
+        string itemType = Console.ReadLine();
+        List<Items> SortedList = expensesList.OrderBy(o => o.Title).OrderBy(o => o.Month).OrderBy(o => o.Amount).ToList();
+        Console.WriteLine("Below is list of Expenses/Incomes sorted by Title, Month and Amount");
+        if (itemType.Trim().ToLower() == "all")
         {
-            Console.WriteLine(items.Id.ToString().PadRight(10) + items.Title.PadRight(10) + items.Month.PadRight(10)
-                + items.Amount.ToString().PadRight(10));
+            Console.WriteLine("Id".PadRight(10) + "Title".PadRight(10) + "Month".PadRight(10)
+                + "Amount".PadRight(10));
+            foreach (Items items in SortedList)
+            {
+                Console.WriteLine(items.Id.ToString().PadRight(10) + items.Title.PadRight(10) + items.Month.PadRight(10)
+                    + items.Amount.ToString().PadRight(10));
+            }
         }
+        else if(itemType.Trim().ToLower() == "expenses")
+        {
+            Console.WriteLine("Id".PadRight(10) + "Title".PadRight(10) + "Month".PadRight(10)
+                + "Amount".PadRight(10));
+            foreach (Items items in SortedList)
+            {
+                if(items.GetType() == typeof(Expense))
+                {
+                    Console.WriteLine(items.Id.ToString().PadRight(10) + items.Title.PadRight(10) + items.Month.PadRight(10)
+                    + items.Amount.ToString().PadRight(10));
+                }  
+            }
+        }
+        else if (itemType.Trim().ToLower() == "incomes")
+        {
+            Console.WriteLine("Id".PadRight(10) + "Title".PadRight(10) + "Month".PadRight(10)
+                + "Amount".PadRight(10));
+            foreach (Items items in SortedList)
+            {
+                if (items.GetType() == typeof(Income))
+                {
+                    Console.WriteLine(items.Id.ToString().PadRight(10) + items.Title.PadRight(10) + items.Month.PadRight(10)
+                    + items.Amount.ToString().PadRight(10));
+                }
+            }
+        }
+
         Items.showOptions();
     }
 
+    //method to add a new expense/income
     public static void addExpense()
     {
         Console.WriteLine("What do you like to enter? Expense/Income?");
@@ -52,6 +90,7 @@ class Items   // base-parent-super class
 		Items.showOptions();
     }
 
+    //method to edit/remove an expense/income
     public static void editRemoveItem()
     {
         Console.WriteLine("What do you want to do? (Edit/Remove)");
@@ -102,17 +141,18 @@ class Items   // base-parent-super class
         Items.showOptions();
     }
 
+    //method to save all the expenses to file and exit
     public static void saveToFile()
     {
-        
-            File.Delete(fileName);
-            IFormatter formatter = new BinaryFormatter();
-            Stream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
-            formatter.Serialize(stream, expensesList);
-            stream.Close();
-		    Console.WriteLine("Data saved successfully to file");
+        File.Delete(fileName);
+        IFormatter formatter = new BinaryFormatter();
+        Stream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+        formatter.Serialize(stream, expensesList);
+        stream.Close();
+		Console.WriteLine("Data saved successfully to file");
     }
 
+    //method to populate the list of expenses/incomes from file on application startup
     public static void populateExpenseList()
     {
         if (File.Exists(fileName))
@@ -126,6 +166,7 @@ class Items   // base-parent-super class
         Items.showOptions();
     }
 
+    //method to show all the available options user can perform in the application
     public static void showOptions()
     {
 		Console.WriteLine("Welcome to TrackMoney");
@@ -136,7 +177,7 @@ class Items   // base-parent-super class
 		Console.WriteLine("(3) Edit item (Edit, Remove)");
 		Console.WriteLine("(4) Save and Quit");
 		string input = Console.ReadLine();
-		switch (input)
+		switch (input.Trim())
 		{
 			case "1":
 				Items.showItems();
@@ -155,6 +196,7 @@ class Items   // base-parent-super class
 		}
     }
 
+    //method to calculate the balance of user
     public static double calcFinalBalance()
     {
         double balance = 0;
